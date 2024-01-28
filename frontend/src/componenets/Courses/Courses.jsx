29bd0682765d411ae9af824ce5a80027c5c7a6f5
Saima-Amin <saimaamin4th2022@gmail.com>
2024-01-28 14:48:37 +0600

    coureses and startlearning commit

--- frontend/src/componenets/Courses/Courses.jsx
+++ frontend/src/componenets/Courses/Courses.jsx
@@ -1,9 +1,48 @@
-// import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    const getApiData = async () => {
+        try {
+            const res = await axios.get('courses.json');
+            setCourses(res.data.courses);
+            // console.log(res.data.courses[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getApiData();
+    }, []);
+
     return (
-        <div>
-            <h3>Top Courses</h3>
+        <div className="container mx-auto mt-24 p-4">
+            <h2 className="text-3xl font-bold mb-12">Top Courses</h2>
+            <div className="grid grid-cols-3 gap-8">
+                {courses.map((course) => {
+                    const { id, title, picture_url, description } = course;
+                    return (
+                        <div
+                            className="border border-teal-400 p-4 rounded-lg text-left max-w-[400px]"
+                            key={id}
+                        >
+                            <img
+                                className="h-48 w-80 mb-2 object-cover rounded"
+                                src={picture_url}
+                                alt=""
+                            />
+                            <h2 className="text-lg font-bold mb-2">Name: {title}</h2>
+                            <p className="text-base">
+                                <span className="text-lg font-semibold">Description:</span>{' '}
+                                {description}
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,8 @@ import {CookiesProvider} from 'react-cookie'
 import Profile from './componenets/Profile/Profile';
 import ProfileHome from './componenets/ProfileHome/ProfileHome';
 import MyLearning from './componenets/MyLearning/MyLearning';
+import Search from './componenets/Search/Search';
+import StartLearning from './componenets/StartLearning/StartLearning';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -37,7 +39,8 @@ const router = createBrowserRouter([
       },
       {
         path:"/search",
-        element: <SearchDetails></SearchDetails>
+        element: <SearchDetails></SearchDetails>,
+        // loader: ({param}) => fetch(`http://127.0.0.1:8000/api/course/`)
       },
       {
         path:"/account",
@@ -45,13 +48,20 @@ const router = createBrowserRouter([
         children:[
           {
             path:"/account/pHome",
-            element: <ProfileHome></ProfileHome>
+            element: <ProfileHome></ProfileHome>,
+            children:[
+              
+            ]
           },
           {
             path:"/account/mylearning",
             element: <MyLearning></MyLearning>,
           }
         ]
+      },
+      {
+        path:"/startlearning",
+        element:<StartLearning></StartLearning>
       },
       // {
       //   path:"/pHome",
